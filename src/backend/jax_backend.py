444a764478129584a465e8f83ce4f994bef0f833
ivan <ivan.peshekhonov@gmail.com>
2022-02-21 18:35:20 +0300

    Add default jax backend

--- src/backend/jax_backend.py
+++ src/backend/jax_backend.py
@@ -1,33 +1,28 @@
-import warnings
-from distutils.version import LooseVersion
-
 try:
     import jax
     from jax.config import config
     config.update("jax_enable_x64", True)
     import jax.numpy as jnp
 except ImportError as error:
-    message = ('Impossible to import Jax.\n'
-               'To use TensorLy with the Jax backend, '
-               'you must first install Jax!')
+    message = ("Impossible to import Jax.\n"
+               "To use Tucker riemopt with the Jax backend, "
+               "you must first install Jax!")
     raise ImportError(message) from error
 
 import numpy
 import copy
 
-from .core import Backend
+from .backend import Backend
 
 
-
-class JaxBackend(Backend, backend_name='jax'):
-
+class JaxBackend(Backend, backend_name="jax"):
     @property
     def type(self):
         return type(self.tensor([]))
 
     @staticmethod
     def context(tensor):
-        return {'dtype': tensor.dtype}
+        return {"dtype": tensor.dtype}
 
     @staticmethod
     def tensor(data, dtype=None, **kwargs):
@@ -62,15 +57,15 @@ class JaxBackend(Backend, backend_name='jax'):
         x, residuals, _, _ = jnp.linalg.lstsq(a, b, rcond=None, numpy_resid=True)
         return x, residuals
 
-    def kr(self, matrices, weights=None, mask=None):
+    def khatri_rao(self, matrices, weights=None, mask=None):
         n_columns = matrices[0].shape[1]
         n_factors = len(matrices)
 
-        start = ord('a')
-        common_dim = 'z'
-        target = ''.join(chr(start + i) for i in range(n_factors))
-        source = ','.join(i + common_dim for i in target)
-        operation = source + '->' + target + common_dim
+        start = ord("a")
+        common_dim = "z"
+        target = "".join(chr(start + i) for i in range(n_factors))
+        source = ",".join(i + common_dim for i in target)
+        operation = source + "->" + target + common_dim
 
         if weights is not None:
             matrices = [m if i else m*self.reshape(weights, (1, -1)) for i, m in enumerate(matrices)]
@@ -92,12 +87,12 @@ class JaxBackend(Backend, backend_name='jax'):
         else:
             return jnp.argsort(tensor, axis=axis)
 
-for name in ['int64', 'int32', 'float64', 'float32', 'complex128', 'complex64', 'reshape', 'moveaxis',
-             'where', 'transpose', 'arange', 'ones', 'zeros', 'flip', 'trace', 'any',
-             'zeros_like', 'eye', 'kron', 'concatenate', 'max', 'min', 'matmul',
-             'all', 'mean', 'sum', 'cumsum', 'count_nonzero', 'prod', 'sign', 'abs', 'sqrt', 'argmin',
-             'argmax', 'stack', 'conj', 'diag', 'clip', 'einsum', 'log2', 'tensordot', 'sin', 'cos', 'squeeze']:
+for name in ["int64", "int32", "float64", "float32", "complex128", "complex64", "reshape",
+             "where", "transpose", "arange", "ones", "zeros", "flip", "trace", "any",
+             "zeros_like", "eye", "kron", "concatenate", "max", "min", "matmul",
+             "all", "mean", "sum", "cumsum", "count_nonzero", "prod", "sign", "abs", "sqrt", "argmin",
+             "argmax", "stack", "conj", "diag", "clip", "einsum", "log2", "sin", "cos", "squeeze"]:
     JaxBackend.register_method(name, getattr(jnp, name))
 
-for name in ['solve', 'qr', 'svd', 'eigh']:
+for name in ["solve", "qr", "svd", "eigh"]:
     JaxBackend.register_method(name, getattr(jnp.linalg, name))
