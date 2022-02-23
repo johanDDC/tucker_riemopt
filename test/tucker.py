648cfe76b3363d200f06d7fb5b57e6ad3314511a
johanDDC <ivan,peshekhonov@gmail.com>
2022-02-23 13:49:23 +0300

    Fix tucker tests

--- test/tucker.py
+++ test/tucker.py
@@ -1,6 +1,6 @@
-import jax.test_util as jtu
-
+from unittest import TestCase
 from src.tucker import Tucker
+import numpy as np
 from src import backend as back
 
 def createTestTensor(n = 4):
@@ -14,13 +14,14 @@ def createTestTensor(n = 4):
         back.einsum("i,j,k->ijk", e, e, x)
     return A
 
-class TuckerTensorTest(jtu.JaxTestCase):
+class TuckerTensorTest(TestCase):
     n = 4
 
     def testFull2Tuck(self):
         A = createTestTensor(self.n)
         A_tuck = Tucker.full2tuck(A, 1e-6)
-        self.assertAllClose(A, A_tuck.full(), rtol=1e-14)
+        assert np.allclose(A, A_tuck.full())
+        # self.assertAllClose(A, A_tuck.full(), rtol=1e-14)
 
     def testAdd(self):
         A = createTestTensor(self.n)
@@ -29,7 +30,8 @@ class TuckerTensorTest(jtu.JaxTestCase):
         self.assertEqual(A2.rank, (4, 4, 4))
         A2 = A2.round(1e-6)
         self.assertEqual(A2.rank, (2, 2, 2))
-        self.assertAllClose((2 * A).full(), A2.full())
+        assert np.allclose((2 * A).full(), A2.full())
+        # self.assertAllClose((2 * A).full(), A2.full())
 
     def testMul(self):
         A = createTestTensor(self.n)
@@ -44,17 +46,20 @@ class TuckerTensorTest(jtu.JaxTestCase):
     def testNorm(self):
         A = createTestTensor(self.n)
         A_tuck = Tucker.full2tuck(A, 1e-6)
-        self.assertAlmostEqual(A_tuck.norm(qr_based=False), back.norm(A), places=4)
-        self.assertAlmostEqual(A_tuck.norm(qr_based=True), back.norm(A), places=4)
+        assert np.allclose(A_tuck.norm(qr_based=False), back.norm(A))
+        assert np.allclose(A_tuck.norm(qr_based=True), back.norm(A))
 
     def testModeProd(self):
         A = createTestTensor(self.n)
         Z = back.zeros((self.n, self.n, self.n))
         A_tuck = Tucker.full2tuck(A, 1e-6)
         M = back.zeros((self.n, self.n))
-        self.assertAllClose(A_tuck.k_mode_product(0, M).full(), Z)
-        self.assertAllClose(A_tuck.k_mode_product(1, M).full(), Z)
-        self.assertAllClose(A_tuck.k_mode_product(2, M).full(), Z)
+        assert np.allclose(A_tuck.k_mode_product(0, M).full(), Z)
+        assert np.allclose(A_tuck.k_mode_product(1, M).full(), Z)
+        assert np.allclose(A_tuck.k_mode_product(2, M).full(), Z)
+        # self.assertAllClose(A_tuck.k_mode_product(0, M).full(), Z)
+        # self.assertAllClose(A_tuck.k_mode_product(1, M).full(), Z)
+        # self.assertAllClose(A_tuck.k_mode_product(2, M).full(), Z)
 
 
 
