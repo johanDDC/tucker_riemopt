0c47e9b5ea34d17fd8a7e99ac6935b3d26105e30
johanDDC <ivan,peshekhonov@gmail.com>
2022-03-03 03:25:48 +0300

    Implement basic riemann gradient descent method

--- src/optimize.py
+++ src/optimize.py
@@ -92,3 +92,75 @@ def get_line_search_tool(line_search_options=None):
             return LineSearchTool.from_dict(line_search_options)
     else:
         return LineSearchTool()
+
+
+def RGD(func: Callable, x_0: Tucker, tolerance=1e-5, max_iter=10000,
+                     line_search_options=None, trace=False):
+    """
+    Performs Riemannian steepest descent optimization method.
+
+    Parameters
+    ----------
+    func : Callable
+        Cost function for minimizing.
+    x_0 : Tucker
+        Starting point for optimization algorithm. Multilinear rank of x_0 defines rank of riemann manifold, on which
+         optimization process is launched.
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    trace : bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['grad_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient on every step of the algorithm
+
+    Example:
+    --------
+    >> oracle = QuadraticOracle(np.eye(5), np.arange(5))
+    >> x_opt, message, history = gradient_descent(oracle, np.zeros(5), line_search_options={'method': 'Armijo', 'c1': 1e-4})
+    >> print('Found optimal point: {}'.format(x_opt))
+       Found optimal point: [ 0.  1.  2.  3.  4.]
+    """
+    history = defaultdict(list)
+    def write_history(func_val, grad_norm):
+        history["func"].append(func_val)
+        history["grad_norm"].append(grad_norm)
+
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = copy(x_0)
+    rank = x_k.rank
+    alpha = line_search_tool.alpha_0
+    riemann_grad = compute_gradient_projection(func, x_k)
+
+    write_history(func(x_k), riemann_grad)
+    iters = 0
+    while history["grad_norm"][-1] ** 2 > tolerance * history["grad_norm"][0] ** 2:
+        alpha = line_search_tool.line_search(func, x_k, -riemann_grad, 2 * alpha)
+        x_k -= alpha * grad_k
+        x_k = x_k.round(rank)
+        grad_k = compute_gradient_projection(func, x_k)
+
+        write_history(func(x_k), grad_k.norm(qr_based=True))
+
+        if np.isnan(history["func"][-1]) or np.isnan(history["grad_norm"][-1]) or \
+                np.isinf(history["func"][-1]) or np.isinf(history["grad_norm"][-1]):
+            raise ValueError("Function or riemann gradient norm values became NaN during optimization process")
+
+        iters += 1
+        if iters >= max_iter:
+            break
+
+    history = history if trace else None
+    return x_k, history
