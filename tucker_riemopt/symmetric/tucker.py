d876b7abec2c5b7fa3a45c5c49500c0a25133e91
johan <ivan.peshekhonov@gmail.com>
2023-04-30 23:44:38 +0300

    Apply new Symmetric Tucker manifold formalism

--- tucker_riemopt/symmetric/tucker.py
+++ tucker_riemopt/symmetric/tucker.py
@@ -24,7 +24,7 @@ ML_rank = Union[int, Sequence[int]]
 class Tucker:
     core: back.type() = field(default_factory=back.tensor)
     common_factors: List[back.type()] = field(default_factory=list)
-    symmetric_modes: List[int] = field(default_factory=list)
+    num_symmetric_modes: int = 0
     symmetric_factor: back.type() = field(default_factory=back.tensor)
 
     @classmethod
@@ -38,15 +38,10 @@ class Tucker:
         """
             Get the tuple representing the shape of Tucker tensor.
         """
-        modes = list(np.arange(0, self.ndim))
         shapes = []
-        curr_common_mode = 0
-        for mode in modes:
-            if mode in self.symmetric_modes:
-                shapes.append(self.symmetric_factor.shape[0])
-            else:
-                shapes.append(self.common_factors[curr_common_mode].shape[0])
-                curr_common_mode += 1
+        for i in range(self.ndim - self.num_symmetric_modes):
+            shapes.append(self.common_factors[i].shape[0])
+        shapes += [self.symmetric_factor.shape[0] for _ in range(self.num_symmetric_modes)]
         return tuple(shapes)
 
     @property
@@ -79,7 +74,7 @@ class Tucker:
         """
             Add two `Tucker` tensors. Result rank is doubled.
         """
-        if self.symmetric_modes != other.symmetric_modes:
+        if self.num_symmetric_modes != other.num_symmetric_modes:
             return self.to_regular_tucker() + other.to_regular_tucker()
         r1 = self.rank
         r2 = other.rank
@@ -95,7 +90,7 @@ class Tucker:
         symmetric_factor = back.concatenate((self.symmetric_factor,
                                                 other.symmetric_factor), axis=1)
         return Tucker(core=core, common_factors=common_factors,
-                      symmetric_modes=self.symmetric_modes, symmetric_factor=symmetric_factor)
+                      num_symmetric_modes=self.num_symmetric_modes, symmetric_factor=symmetric_factor)
 
     def __rmul__(self, a):
         """
@@ -103,7 +98,7 @@ class Tucker:
         """
         new_tensor = deepcopy(self)
         return Tucker(a * new_tensor.core, new_tensor.common_factors,
-                      new_tensor.symmetric_modes, new_tensor.symmetric_factor)
+                      self.num_symmetric_modes, new_tensor.symmetric_factor)
 
     def __neg__(self):
         return (-1) * self
@@ -116,8 +111,8 @@ class Tucker:
         """
             Calculate inner product of given `Tucker` tensors.
         """
-        if self.symmetric_modes != other.symmetric_modes:
-            return self.to_regular_tucker() + other.to_regular_tucker()
+        if self.num_symmetric_modes != other.num_symmetric_modes:
+            return self.to_regular_tucker().flat_inner(other.to_regular_tucker())
         factors = []
         transposed_factors = []
         core_letters = ascii_letters[:self.ndim]
@@ -125,21 +120,19 @@ class Tucker:
         transposed_letters = []
         intermediate_core_letters = []
         symmetric_factor = other.symmetric_factor.T @ self.symmetric_factor
-        cur_common_mode = 0
         rev_letters = ascii_letters[self.ndim:][::-1]
         for i in range(1, self.ndim + 1):
             j = i - 1
-            if j in self.symmetric_modes:
+            if j < self.ndim - self.num_symmetric_modes - 1: # FIXME -1
+                factors.append(self.common_factors[j])
+                factors_letters.append(ascii_letters[self.ndim + i] + core_letters[j])
+                transposed_factors.append(other.common_factors[j].T)
+                transposed_letters.append(rev_letters[j] + ascii_letters[self.ndim + i])
+                intermediate_core_letters.append(rev_letters[j])
+            else:
                 factors.append(symmetric_factor)
                 factors_letters.append(ascii_letters[self.ndim + i] + core_letters[j])
                 intermediate_core_letters.append(ascii_letters[self.ndim + i])
-            else:
-                factors.append(self.common_factors[cur_common_mode])
-                factors_letters.append(ascii_letters[self.ndim + i] + core_letters[j])
-                transposed_factors.append(other.common_factors[cur_common_mode].T)
-                transposed_letters.append(rev_letters[j] + ascii_letters[self.ndim + i])
-                intermediate_core_letters.append(rev_letters[j])
-                cur_common_mode += 1
 
         source = ",".join([core_letters] + factors_letters + transposed_letters)
         intermediate_core = back.einsum(source + "->" + "".join(intermediate_core_letters),
@@ -159,10 +152,8 @@ class Tucker:
         """
         if k < 0 or k >= self.ndim:
             raise ValueError(f"k shoduld be from 0 to {self.ndim - 1}")
-        if k in self.symmetric_modes:
+        if k >= self.ndim - self.num_symmetric_modes - 1: # FIXME -1
             return self.to_regular_tucker().k_mode_product(k, mat)
-        k_change = sum(1 for m in self.symmetric_modes if k > m)
-        k -= k_change
         new_tensor = deepcopy(self)
         new_tensor.common_factors[k] = mat @ new_tensor.common_factors[k]
         return new_tensor
@@ -196,7 +187,7 @@ class Tucker:
                 common_factors.append(back.qr(self.common_factors[i])[1])
             symmetric_factor = back.qr(self.symmetric_factor)[1]
             new_tensor = Tucker(self.core, common_factors,
-                                self.symmetric_modes, symmetric_factor)
+                                self.num_symmetric_modes, symmetric_factor)
             new_tensor = new_tensor.full()
             return back.norm(new_tensor)
 
@@ -211,28 +202,19 @@ class Tucker:
         tensor_letters = ""
         factors = []
         curr_common_factor = 0
-        for i in range(self.ndim):
+        for i in range(self.ndim - self.num_symmetric_modes):
             factor_letters += f"{ascii_letters[self.ndim + i]}{ascii_letters[i]},"
             tensor_letters += ascii_letters[self.ndim + i]
-            if i in self.symmetric_modes:
-                factors.append(self.symmetric_factor)
-            else:
-                factors.append(self.common_factors[curr_common_factor])
-                curr_common_factor += 1
-
+            factors.append(self.common_factors[i])
+        factors += [self.symmetric_factor for _ in range(self.num_symmetric_modes)]
         einsum_str = core_letters + "," + factor_letters[:-1] + "->" + tensor_letters
         return back.einsum(einsum_str, self.core, *factors)
 
     def to_regular_tucker(self):
-        modes = list(np.arange(0, self.ndim))
         factors = []
-        curr_common_mode = 0
-        for mode in modes:
-            if mode in self.symmetric_modes:
-                factors.append(self.symmetric_factor)
-            else:
-                factors.append(self.common_factors[curr_common_mode])
-                curr_common_mode += 1
+        for i in range(self.ndim - self.num_symmetric_modes):
+            factors.append(self.common_factors[i])
+        factors += [self.symmetric_factor for _ in range(self.num_symmetric_modes)]
         return RegularTucker(core=self.core, factors=factors)
 
     def __deepcopy__(self, memodict={}):
@@ -240,7 +222,7 @@ class Tucker:
         common_factors = [back.copy(factor) for factor in self.common_factors]
         symmetic_factor = back.copy(self.symmetric_factor)
         return self.__class__(new_core, common_factors,
-                              deepcopy(self.symmetric_modes), symmetic_factor)
+                              self.num_symmetric_modes, symmetic_factor)
 
 
 TangentVector = Tucker
