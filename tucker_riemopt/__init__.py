d1b47cddd03d240b9c67153b5b72fa585086e39b
johan <ivan.peshekhonov@gmail.com>
2023-06-15 01:40:53 +0300

    Add dependency of tucker riemannian components to package init. Refactor.

--- tucker_riemopt/__init__.py
+++ tucker_riemopt/__init__.py
@@ -1,6 +1,7 @@
-from .backend import (set_backend, get_backend)
-
 from . import backend
 
-from .tucker import Tucker, SparseTensor
+from backend import (set_backend, get_backend)
+
+from tucker_riemopt.tucker.tucker import Tucker, SparseTensor
 from .matrix import TuckerMatrix
+from tucker_riemopt.tucker import riemannian as TuckerRiemannian
