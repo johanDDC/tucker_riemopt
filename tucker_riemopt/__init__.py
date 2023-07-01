2c6a21c3b784b10f8c64629b1faeb9444c4db25b
johan <ivan.peshekhonov@gmail.com>
2023-07-01 08:52:01 +0300

    Fix imports

--- tucker_riemopt/__init__.py
+++ tucker_riemopt/__init__.py
@@ -3,5 +3,9 @@ from . import backend
 from .backend import (set_backend, get_backend)
 
 from tucker_riemopt.tucker.tucker import Tucker, SparseTensor
+from tucker_riemopt.sf_tucker.sf_tucker import SFTucker
+
 from .matrix import TuckerMatrix
+
 from tucker_riemopt.tucker import riemannian as TuckerRiemannian
+from tucker_riemopt.sf_tucker import riemannian as SFTuckerRiemannian
