79c7d3fdae11267a16c2d1c79e13a9fba1fa448d
johan <ivan.peshekhonov@gmail.com>
2023-07-01 07:10:50 +0300

    Fix backend

--- tucker_riemopt/__init__.py
+++ tucker_riemopt/__init__.py
@@ -1,6 +1,6 @@
 from . import backend
 
-from backend import (set_backend, get_backend)
+from .backend import (set_backend, get_backend)
 
 from tucker_riemopt.tucker.tucker import Tucker, SparseTensor
 from .matrix import TuckerMatrix
