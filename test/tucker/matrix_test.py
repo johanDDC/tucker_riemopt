ffb98b42e9f551a29c495cf8411f4b79cbaa8924
johan <ivan.peshekhonov@gmail.com>
2023-09-28 20:34:53 +0300

    Implement more complicated matrix test

--- test/tucker/matrix_test.py
+++ test/tucker/matrix_test.py
@@ -9,21 +9,25 @@ from tucker_riemopt import backend as back
 
 class TuckerMatrixTest(TestCase):
     def testDenseMatmul(self):
-        eye = back.eye(8)
-        eye = back.reshape(eye, (2, 2, 2, 2, 2, 2), order="F")
-        eye = back.transpose(eye, (0, 3, 1, 4, 2, 5))
-        eye = back.reshape(eye, (4, 4, 4), order="F")
-        eye = TuckerMatrix.from_dense(eye, (2, 2, 2), (2, 2, 2))
-        x = back.randn((2, 2, 2))
-        y = eye @ x
-        assert np.allclose(back.to_numpy(y), back.to_numpy(x))
+        matrix_dense = back.randn((8, 8))
+        x_dense = back.randn((8,))
+        x = back.reshape(x_dense, (2, 2, 2))
+        y_dense = matrix_dense @ x_dense
+        matrix_dense = back.reshape(matrix_dense, (2, 2, 2, 2, 2, 2))
+        matrix_dense = back.transpose(matrix_dense, (0, 3, 1, 4, 2, 5))
+        matrix_dense = back.reshape(matrix_dense, (4, 4, 4))
+        matrix_tucker = TuckerMatrix.from_dense(matrix_dense, (2, 2, 2), (2, 2, 2))
+        y = matrix_tucker @ x
+        assert np.allclose(back.to_numpy(y_dense), back.to_numpy(back.reshape(y, (8,))))
 
     def testDenseBatchMatmul(self):
-        eye = back.eye(8)
-        eye = back.reshape(eye, (2, 2, 2, 2, 2, 2), order="F")
-        eye = back.transpose(eye, (0, 3, 1, 4, 2, 5))
-        eye = back.reshape(eye, (4, 4, 4), order="F")
-        eye = TuckerMatrix.from_dense(eye, (2, 2, 2), (2, 2, 2))
-        x = back.randn((10, 2, 2, 2))
-        y = eye @ x
-        assert np.allclose(back.to_numpy(y), back.to_numpy(x))
+        matrix_dense = back.randn((8, 8))
+        x_dense = back.randn((10, 8))
+        y_dense = (matrix_dense @ x_dense.T).T
+        x = back.reshape(x_dense, (10, 2, 2, 2))
+        matrix_dense = back.reshape(matrix_dense, (2, 2, 2, 2, 2, 2))
+        matrix_dense = back.transpose(matrix_dense, (0, 3, 1, 4, 2, 5))
+        matrix_dense = back.reshape(matrix_dense, (4, 4, 4))
+        matrix_tucker = TuckerMatrix.from_dense(matrix_dense, (2, 2, 2), (2, 2, 2))
+        y = matrix_tucker @ x
+        assert np.allclose(back.to_numpy(y_dense), back.to_numpy(back.reshape(y, (10, 8))), atol=1e-5)
